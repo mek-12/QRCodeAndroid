6ae00440e378614b4331f3c25ecef52f02461266
Muhammed Emin KARABOGA <mek.krbg04@gmail.com>
2019-03-01 17:48:32 +0300

    ORCode commit 2

--- QrCode/app/src/main/java/com/example/androdiqrcodefirebase/MainActivity.java
+++ QrCode/app/src/main/java/com/example/androdiqrcodefirebase/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.androdiqrcodefirebase;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.androdiqrcodefirebase.Helper.GraphicOverlay;
+import com.example.androdiqrcodefirebase.Helper.RectOverlay;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.wonderkiln.camerakit.CameraKitError;
 import com.wonderkiln.camerakit.CameraKitEvent;
 import com.wonderkiln.camerakit.CameraKitEventListener;
 import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -24,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     AlertDialog waitingDialog;
     @Bind(R.id.btn_detect) Button detectBttn;
     @Bind(R.id.camera_view) CameraView cameraView;
-
+    @Bind(R.id.graphic_overlay) GraphicOverlay graphicOverlay;
     @OnClick(R.id.btn_detect) void detectClick(){
         cameraView.start();
         cameraView.captureImage();
@@ -63,6 +80,8 @@ public class MainActivity extends AppCompatActivity {
                 Bitmap bitmap = cameraKitImage.getBitmap();
                 bitmap = Bitmap.createScaledBitmap(bitmap, cameraView.getWidth(),cameraView.getHeight(),false);
                 cameraView.stop();
+                
+                runDetector(bitmap);
             }
 
             @Override
@@ -82,5 +101,97 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause(){
         super.onPause();
         cameraView.stop();
+    }
+
+    private void runDetector(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionBarcodeDetectorOptions options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                .setBarcodeFormats(
+                        FirebaseVisionBarcode.FORMAT_QR_CODE,
+                        FirebaseVisionBarcode.FORMAT_PDF417
+                )
+                .build();
+        FirebaseVisionBarcodeDetector detector = FirebaseVision
+                .getInstance()
+                .getVisionBarcodeDetector(options);
+
+
+        detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                            processResult(firebaseVisionBarcodes);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(),  Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+
+        for (FirebaseVisionBarcode item : firebaseVisionBarcodes)
+        {
+            // Draw Rect
+
+            Rect rectBounds = item.getBoundingBox();
+            RectOverlay rectOverlay = new RectOverlay(graphicOverlay,rectBounds);
+            graphicOverlay.add(rectOverlay);
+
+            int value_type = item.getValueType();
+            switch (value_type){
+                case FirebaseVisionBarcode.TYPE_TEXT:
+                {
+                    android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
+                    builder.setMessage(item.getRawValue());
+                    builder.setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int which) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                    android.support.v7.app.AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                break;
+                case FirebaseVisionBarcode.TYPE_URL:
+                {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getRawValue()));
+                    startActivity(intent);
+                }
+                break;
+
+                case FirebaseVisionBarcode.TYPE_CONTACT_INFO:
+                {
+                    String info = new StringBuilder()
+                            .append("Name: " + item.getContactInfo().getName().getFormattedName())
+                            .append("\n")
+                            .append("Address: " + item.getContactInfo().getAddresses().get(0).getAddressLines())
+                            .append("\n")
+                            .append("Email: " + item.getContactInfo().getEmails().get(0).getAddress())
+                            .toString();
+
+                    android.support.v7.app.AlertDialog.Builder builder = new android.support.v7.app.AlertDialog.Builder(this);
+                    builder.setMessage(info);
+                    builder.setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int which) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                    android.support.v7.app.AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                break;
+                default:
+                    break;
+            }
+        }
+        waitingDialog.dismiss();
     }
 }
